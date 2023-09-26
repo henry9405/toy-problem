803cb64a6a384ce327da96d9327ca039f3d725e9
henry9405 <144359211+henry9405@users.noreply.github.com>
2023-09-26 11:59:06 +0300

    initials

--- grade.js
+++ grade.js
@@ -1,1 +1,24 @@
+function calculateGrade() {
+    const marks = parseFloat(prompt("Enter student marks (0-100):")); //parseFloat converts string to a number
+    
+    if (!isNaN(marks) && marks >= 0 && marks <= 100) {
+        if (marks > 79) {
+            return 'A';
+        } else if (marks >= 60) {
+            return 'B';
+        } else if (marks >= 50) {
+            return 'C';
+        } else if (marks >= 40) {
+            return 'D';
+        } else {
+            return 'E';
+        }
+    } else {
+        return 'Invalid input. Please enter a number between 0 and 100.';
+    }
+}
+
+const grade = calculateGrade();
+console.log(`The student's grade is: ${grade}`);
+
 
--- speed.js
+++ speed.js
@@ -1,1 +1,12 @@
-
+function theDemeritPoints(speed){
+    if (speed < 70){
+        console.log("OK");
+    } else {
+        const demeritPoints = Math.floor((speed - 70) / 5);
+        if (demeritPoints > 12) {
+            console.log("License suspended")
+        } else {
+            console.log("Points:", demeritPoints);
+        }
+    }
+}
